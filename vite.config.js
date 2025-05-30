d9f5207968a312ad0b6ca431cd2a82c97aa1ac1c
AhmetYilmaz535 <91874825+AhmetYilmaz535@users.noreply.github.com>
2025-05-30 12:34:52 +0300

    Configure for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/homelytreasures/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true,
-  },
-  server: {
-    port: process.env.PORT || 3000,
-    host: '0.0.0.0'
   }
 });
