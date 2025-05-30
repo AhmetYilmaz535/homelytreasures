5a1ae4b2ad850b32b5518e6aeee7ea6e959dc9e8
AhmetYilmaz535 <91874825+AhmetYilmaz535@users.noreply.github.com>
2025-05-30 12:28:54 +0300

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  base: '/homelytreasures/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
